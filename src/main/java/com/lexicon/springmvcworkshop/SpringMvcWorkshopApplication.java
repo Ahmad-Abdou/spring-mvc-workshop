ebe9417818a446e8f5b4f1efbc2b4eb78bf8770e
Ahmad-Abdou <47502597+Ahmad-Abdou@users.noreply.github.com>
2021-03-24 09:15:01 +0100

    project created

---
+++ src/main/java/com/lexicon/springmvcworkshop/SpringMvcWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.lexicon.springmvcworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lexicon/springmvcworkshop/SpringMvcWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexicon.springmvcworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
